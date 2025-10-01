8f73c3ec5f56b42342a979e9c5ddb241bff3d663
phzineasygame <pedroflorencio@colegiopaulodetarso.com.br>
2025-10-01 16:23:38 +0000

    miwa_theGoat

--- ex2.py
+++ ex2.py
@@ -1,8 +1,10 @@
 def avalieAnimes():
-    mikadono_sisters = input("Avalie Mikadono Sisters (de 0 a 10): "))
-    horimiya = input("Avalie Horimiya (de 0 a 10): "))
-    the_quintuplets = input("Avalie The Quintuplets (de 0 a 10): "))
-
+    mikadono_sisters = float(input("Avalie Mikadono Sisters (de 0 a 10): "))
+    horimiya = float(input("Avalie Horimiya (de 0 a 10): "))
+    the_quintuplets = float(input("Avalie The Quintuplets (de 0 a 10): "))
+    the_angel_next_door_spoils_me_rotten = float(input("Avalie The Angel Next Door(de 0 a 10): "))
+    relife = float(input("Avalie relife (de 0 a 10): "))
+    alya_sometimes_hides_her_feelings_in_russian = float(input("Avalie Alya Sometimes hides her feelings in russian (de 0 a 10): "))
     if mikadono_sisters > 6:
         print("Mikadono Sisters: Anime bom!")
     else:
@@ -17,3 +19,16 @@ def avalieAnimes():
         print("The Quintuplets: Anime bom!")
     else:
         print("The Quintuplets: Anime ruim...")
+    if the_angel_next_door_spoils_me_rotten > 6:
+        print("The_angel_next_door_spoils_me_rotten: Anime bom!")
+    else:
+        print("The_angel_next_door_spoils_me_rotten: Anime ruim...!")
+    if relife > 6:
+        print("relife: Anime bom!")
+    else:
+        print("relife: Anime ruim...")
+    if alya_sometimes_hides_her_feelings_in_russian > 6:
+        print("alya_sometimes_hides_her_feelings_in_russian: Anime bom!")
+    else:
+        print("alya_sometimes_hides_her_feelings_in_russian: Anime ruim...")
+avalieAnimes()
---
+++ ex3.py
@@ -0,0 +1,12 @@
+valores = [
+    10,
+     3 + 2j,
+     7 + 5j,
+     69 + 8j,
+    30
+    t = 1000 - 500,
+
+]
+
+for valor in valores:
+    print(f"Valor: {valor} - Tipo: {type(valor)}")
