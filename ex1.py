6a1d675e2d9e56e7ba216af812604428ae555c87
phzineasygame <pedroflorencio@colegiopaulodetarso.com.br>
2025-10-01 15:24:31 +0000

    miwaaa

---
+++ ex1.py
@@ -0,0 +1,13 @@
+def assistirAnimeSisters(personagens):
+    print(f"vamos assistir mikadono sisters!")
+
+personagens = input("Me fale qual sua irmã favorita!:  ")
+assistirAnimeSisters(personagens)
+
+if personagens == "Niko":
+    print("é a irmã do meio")
+elif personagens == "Miwa":
+    print("é a irmã mais nova")
+elif personagens == "kazuki":
+    print("é a irmã mais velha")
+
